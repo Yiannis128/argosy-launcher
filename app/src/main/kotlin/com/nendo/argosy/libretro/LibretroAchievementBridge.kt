573d589445926976d0fd923baaecfa62a7fe0835
nendo <tmgast@gmail.com>
2026-04-30 18:55:08 +0900

    Update LibretroAchievementBridge AchievementUpdateBus import to core.event package

--- app/src/main/kotlin/com/nendo/argosy/libretro/LibretroAchievementBridge.kt
+++ app/src/main/kotlin/com/nendo/argosy/libretro/LibretroAchievementBridge.kt
@@ -9,7 +9,7 @@ import com.nendo.argosy.domain.usecase.achievement.VerifyRAGameIdUseCase
 import com.nendo.argosy.hardware.AmbientLedManager
 import com.nendo.argosy.libretro.ui.AchievementUnlock
 import com.nendo.argosy.libretro.ui.RAConnectionInfo
-import com.nendo.argosy.ui.screens.common.AchievementUpdateBus
+import com.nendo.argosy.core.event.AchievementUpdateBus
 import com.swordfish.libretrodroid.GLRetroView
 import kotlinx.coroutines.CoroutineScope
 
