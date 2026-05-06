21bd943859c5e72577d98fbcb96fb7bd8c11d42f
peanut250 <209579534+peanut250@users.noreply.github.com>
2026-05-05 22:18:53 -0500

    feat: Double tap to toggle quick menu overlay (#182)
    * Double tap to toggle quick menu overlay
    
    * Fix quick menu overlay interactable invisible components
    
    * Fix quick menu overlay interactable invisible components - preserve alpha anim
    
    * Fix quick menu overlay interactable invisible components

--- app/src/main/kotlin/com/nendo/argosy/ui/quickmenu/components/QuickMenuContent.kt
+++ app/src/main/kotlin/com/nendo/argosy/ui/quickmenu/components/QuickMenuContent.kt
@@ -36,7 +36,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -45,7 +44,6 @@ import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.nendo.argosy.ui.common.rememberFileImageModel
 import com.nendo.argosy.ui.quickmenu.GameCardUi
